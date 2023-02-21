507a3dde3e7b99faed160592b2125017e2d478c0
marijanikolovski <m.kuzeljevic@gmail.com>
2023-02-21 13:04:53 +0100

    Data Types And Converting Row Values

--- build/index.js
+++ build/index.js
@@ -1,21 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResults_1 = require("./MatchResults");
 const reader = new CsvFileReader_1.CsvFileReader('football.csv');
 reader.read();
-// enum - enumeration
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult || (MatchResult = {}));
 let manUnitedWins = 0;
 for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    if (match[1] === 'Man United' && match[5] === MatchResults_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    else if (match[2] === 'Man United' && match[5] === MatchResults_1.MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
